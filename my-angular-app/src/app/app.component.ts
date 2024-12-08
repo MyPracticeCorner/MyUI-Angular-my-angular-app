bb97a68478c22b60cc043b168d25582c5be02e5e
sachinidea45 <sachinidea45@gmail.com>
2024-12-08 22:11:36 +0530

    Changes for form data

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, HomeComponent, ThankYouComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isContactFormSubmitted = false;
   title = 'my-angular-app from sachinidea45 ng build --prod';
+
+  resetIsContactFormSubmitted() {
+    this.isContactFormSubmitted = !(this.isContactFormSubmitted);
+    let messageTextElement = document.getElementById('messageTextElement') as HTMLElement;
+    messageTextElement?.style.setProperty('display', 'block')
+  }
 }
--- my-angular-app/src/app/app.routes.ts
+++ my-angular-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+  { path: '', component: HomeComponent },
+  { path: 'thank-you', component: ThankYouComponent },
+];
+
